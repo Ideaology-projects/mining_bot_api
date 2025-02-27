5f9ca5f69c7c4fe501080d61f26d513f2622677a
Asim khan <asimabas95@gmail.com>
2025-02-27 15:16:05 +0500

    add changes in build

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ app.use('/api/v1/rewards', rewardRoutes);
 app.use('/api/v1/refferal', refferalRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
