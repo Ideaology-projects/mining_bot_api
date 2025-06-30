785e88367658de56c4ebc2a0668e69168e4cddc8
AsimKhanMetaMelon <asimkhan@metamelon.org>
2025-06-30 17:24:56 +0500

    get entries api

--- src/routes/referral.routes.ts
+++ src/routes/referral.routes.ts
@@ -1,14 +1,13 @@
 import { Router } from 'express';
 import {
   getReferralStatus,
-  claimReferralRewards,
 } from '../controllers/referral.controller';
-import { claimReferralReward,isClaimedReward } from '../controllers/claimedReferralReward.controller';
+import { claimReferralReward,isClaimedReward} from '../controllers/claimedReferralReward.controller';
 const router = Router();
 import { authMiddleware } from '../middlewares/auth.middleware';
 
 router.get('/referral-status', authMiddleware, getReferralStatus);
-router.post('/claim', authMiddleware, claimReferralRewards);
+router.post('/claim', authMiddleware, claimReferralReward);
 router.post('/is-claimed', authMiddleware, isClaimedReward);
 
 //Referral Reward Route
