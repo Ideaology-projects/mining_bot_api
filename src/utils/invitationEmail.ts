b30e9785cfa4422ffe307e948ba85bdef91058e3
AsimKhanMetaMelon <asimkhan@metamelon.org>
2025-10-03 12:45:56 +0500

    syncPassword

--- src/utils/invitationEmail.ts
+++ src/utils/invitationEmail.ts
@@ -48,15 +48,22 @@ async function sendEmail({
   }
 }
 
+
 const sendResetEmail = async (email: string, resetToken: string, otp: string) => {
+  const resetLink = `https://vbtc.co/my-account/lost-password/?email=${email}&token=${resetToken}`;
   
   await transporter.sendMail({
     from: `"Support" <${process.env.EMAIL_USER}>`,
     to: email,
     subject: 'Password Reset',
-    html: `<p>You requested to reset your password. Use the following OTP code to proceed:</p>
-    <h2>${otp}</h2>`
+    html: `
+      <p>You requested to reset your password. Use the following OTP code to proceed (for game):</p>
+      <h2>${otp}</h2>
+      <p>Or reset your password on the website by clicking the link below:</p>
+      <a href="${resetLink}" target="_blank">${resetLink}</a>
+    `
   });
 };
+
 
 export { sendEmail, sendResetEmail };
